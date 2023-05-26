3e0a11583fc93bc868144c7f7f11fc1cc4385974
aluno <aluno@ubunturails.myguest.virtualbox.org>
2023-05-26 10:54:52 -0300

    atualizou_a tabela

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :users
   resources :estoques
   resources :vendas
   resources :clientes
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_19_115728) do
+ActiveRecord::Schema.define(version: 2023_05_26_113332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,13 @@ ActiveRecord::Schema.define(version: 2023_05_19_115728) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["fornecedor_id"], name: "index_produtos_on_fornecedor_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "nome"
+    t.string "telefone"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "vendas", force: :cascade do |t|
