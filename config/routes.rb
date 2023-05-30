a2ced2196f18957c280f55a0e3a35a25604cf4c1
aluno <aluno@ubunturails.myguest.virtualbox.org>
2023-05-30 10:15:39 -0300

    termino-desing-projeto-e termino tabela user

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :clientes
   resources :produtos
   resources :fornecedors
+  root to:'produtos#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
